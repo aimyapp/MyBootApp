02fa892645c471fe02f58d85291317c6a584cb03
aimy <aimy.app@gmail.com>
2019-12-31 03:11:30 +0900

    refs:List3-10 helo page追加

--- src/main/java/com/tuyano/springboot/HeloController.java
+++ src/main/java/com/tuyano/springboot/HeloController.java
@@ -1,36 +1,13 @@
 package com.tuyano.springboot;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import lombok.Data;
-
-@RestController
+@Controller
 public class HeloController {
 
-	String[] names = { "tuyano", "hanako", "taro", "sachiko", "ichiro" };
-	String[] mails = { "syodaptuyano.com", "hanako@flower", "taro@yamada", "sachiko@happy", "ichiro@baseball" };
-
-	@RequestMapping("/{id}")
-	public DataObject index(@PathVariable int id) {
-		return new DataObject(id, names[id], mails[id]);
+	@RequestMapping("/")
+	public String index() {
+		return "index";
 	}
-}
-
-@Data
-class DataObject {
-	private int id;
-	private String name;
-	private String value;
-
-	public DataObject(int id, String name, String value) {
-
-		super();
-		this.id = id;
-		this.name = name;
-		this.value = value;
-
-	}
-
 }
