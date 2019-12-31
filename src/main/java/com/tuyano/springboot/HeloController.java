e4431354417717e40f778b6b3594f7d7e959d1b2
aimy <aimy.app@gmail.com>
2020-01-01 02:15:03 +0900

    refs:List4-7_8 table作成

--- src/main/java/com/tuyano/springboot/HeloController.java
+++ src/main/java/com/tuyano/springboot/HeloController.java
@@ -2,45 +2,36 @@ package com.tuyano.springboot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import lombok.Data;
 
 @Controller
 public class HeloController {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView index(ModelAndView mav) {
+	@RequestMapping(value = "/")
+	public ModelAndView send(ModelAndView mav) {
 		mav.setViewName("index");
-		mav.addObject("msg", "フォームを送信してください。");
+		mav.addObject("msg", "current data.");
+		DataObject obj = new DataObject(123, "hanako", "hanako@flower");
+		mav.addObject("object", obj);
 		return mav;
 	}
+}
 
-	@RequestMapping(value = "/", method = RequestMethod.POST)
-	public ModelAndView send(
-			@RequestParam(value = "check1", required = false) boolean check1,
-			@RequestParam(value = "radio1", required = false) String radio1,
-			@RequestParam(value = "select1", required = false) String select1,
-			@RequestParam(value = "select2", required = false) String[] select2,
-			ModelAndView mav) {
+@Data
+class DataObject {
+	private int id;
+	private String name;
+	private String value;
 
-		String res = "";
-		try {
-			res = "check:" + check1 +
-					" radio:" + radio1 +
-					" select:" + select1 +
-					"\nselect2:";
-		} catch (NullPointerException e) {
-		}
-		try {
-			res += select2[0];
-			for (int i = 1; i < select2.length; i++)
-				res += ", " + select2[i];
-		} catch (NullPointerException e) {
-			res += "null";
-		}
-		mav.addObject("msg", res);
-		mav.setViewName("index");
-		return mav;
+	public DataObject(int id, String name, String value) {
+
+		super();
+		this.id = id;
+		this.name = name;
+		this.value = value;
+
 	}
+
 }
